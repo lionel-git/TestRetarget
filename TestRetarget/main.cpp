292d94c0aaa1b340fb961af19da86cb1c806d35e
lionel-git <lionel.desorme@free.fr>
2019-05-26 15:12:45 +0200

    Add var for sdk version

--- TestRetarget/main.cpp
+++ TestRetarget/main.cpp
@@ -3,5 +3,5 @@
 int main(int argc, char** argv)
 {
 	std::cout << "Hello world" << std::endl;
-	std::cout << _MSC_VER << " " << _WIN32_WINNT << std::endl;
+	std::cout << _MSC_VER << " " << _WIN32_WINNT << " " << NTDDI_VERSION << std::endl;
 }
--- TestRetarget3/main.cpp
+++ TestRetarget3/main.cpp
@@ -4,5 +4,5 @@
 int main(int argc, char** argv)
 {
 	std::cout << "Hello world" << std::endl;
-	std::cout << _MSC_VER <<  " " << _WIN32_WINNT << std::endl;
+	std::cout << _MSC_VER <<  " " << _WIN32_WINNT << " " << WDK_NTDDI_VERSION << " " << NTDDI_VERSION << std::endl;
 }
