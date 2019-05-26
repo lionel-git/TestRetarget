c539272a653c9567c808b7a3f2a5ede19a9d7273
lionel-git <lionel.desorme@free.fr>
2019-05-26 14:59:18 +0200

    Add 2 test projects

--- TestRetarget3/main.cpp
+++ TestRetarget3/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <sdkddkver.h>
+
 int main(int argc, char** argv)
 {
 	std::cout << "Hello world" << std::endl;
-	std::cout << _MSC_VER << " " << _WIN32_WINNT << std::endl;
+	std::cout << _MSC_VER <<  " " << _WIN32_WINNT << std::endl;
 }
